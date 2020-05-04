c6b78cb00733550487d44757a8f30f2372265789
Leslie Samuel Kingsly <lkingsly@sapient.com>
2020-05-04 21:08:47 +0530

    Git Submodule test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React / CI / CD / Docker / Kubernetes From Feature
+          Learn React / CI / CD / Docker / Kubernetes
         </a>
       </header>
     </div>
